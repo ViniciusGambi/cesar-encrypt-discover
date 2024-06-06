08d36dd7a27ad47df2d824a2b1794f5c8b9b882c
Vinicius Gambi <viniciusgambi@gmail.com>
2024-06-05 21:58:03 -0300

    cesar fix

--- cesar-cipher-discover.py
+++ cesar-cipher-discover.py
@@ -1,4 +1,4 @@
-#from langdetect import detect, DetectorFactory
+from langdetect import detect, DetectorFactory
 
 text = "Hzn oerir erivfnb fboer pevcgbtensvn"
 
@@ -13,13 +13,13 @@ def cesar_encrypt(text, n):
             result += chr((ord(char) + n - 97) % 26 + 97)
     return result
 
-#def check_if_sentence_makes_sense_in_portuguese(sentence):
-#    DetectorFactory.seed = 0
-#    language = detect(sentence)
-#    return language == 'pt'
+def check_if_sentence_makes_sense_in_portuguese(sentence):
+    DetectorFactory.seed = 0
+    language = detect(sentence)
+    return language == 'pt'
 
 if __name__ == '__main__':
     for i in range(26):
         sentence = cesar_encrypt(text, i)
-        #if check_if_sentence_makes_sense_in_portuguese(sentence):
-        print(i, sentence)
+        if check_if_sentence_makes_sense_in_portuguese(sentence):
+            print(i, sentence)
